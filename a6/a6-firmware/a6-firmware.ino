17cab0c2187419bd5540e5ab9c7a2876c9ebaa85
sahanavishwanath <sahanav@uw.edu>
2019-03-04 16:31:13 -0800

    Adding a8 files

--- a6/a6-firmware/a6-firmware.ino
+++ a6/a6-firmware/a6-firmware.ino
@@ -1,13 +1,13 @@
 // analog input pin that the photoresistor is attached to
 const int analogInPin = A1;
 // value read from the photoresistor
-int sensorValue = 0;
+ int sensorValue = 0;
 
 void setup() {
   // initialize serial communications at 9600 bps
   Serial.begin(9600);
-  // set analogInPin A0 as an input
-  pinMode(analogInPin, INPUT);
+  // set analogInPin A1 as an input
+  // pinMode(analogInPin, INPUT);
 }
 
 void loop() {
@@ -17,4 +17,5 @@ void loop() {
   Serial.print("The sensor value is ");
   // prints the sensorValue to the serial monitor                
   Serial.println(sensorValue);
+  delay(50);
 }
